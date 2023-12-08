435d9a132511c801c82331990ba1095e9beda3c1
kashanjaved <106954455+kashanjaved@users.noreply.github.com>
2023-12-09 00:20:08 +0500

    upload

--- Flyweight/TreeFactory.java
+++ Flyweight/TreeFactory.java
@@ -14,14 +14,14 @@ public class TreeFactory {
 
     public static TreeType getTreeType(String name, Color color, String otherTreeData,
                                        boolean isFruitTree, int countOfFruits) {
-        // Generate a unique key for each TreeType based on name and color
+        
         String key = name + color.getRGB();
 
-        // Check if the TreeType with the given key already exists
+    
         TreeType result = treeTypes.get(key);
 
         if (result == null) {
-            // If not, create a new TreeType and store it in the map
+           
             result = new TreeType(name, color, otherTreeData, isFruitTree, countOfFruits);
             treeTypes.put(key, result);
         }
